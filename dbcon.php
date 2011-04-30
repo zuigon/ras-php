c7a7e6ed00184c0ac014ccf106db228d5fa3d1fd
BKrsta <bkrsta@gmail.com>
2011-05-01 00:40:08 +0200

    ...

--- dbcon.php
+++ dbcon.php
@@ -2,16 +2,12 @@
 
 $c = "db.php";
 $cc = "../db.php";
-if(!file_exists($c))
-  if(file_exists($cc))
-    $c = $cc;
-  else die("Error: No db.conf found!");
+if(!file_exists($c)) if(file_exists($cc)) $c = $cc;
+else die("Error: No db.conf found!");
+unset($cc);
 
-// $f = file_get_contents(realpath($c));
-// $cc = explode("\n", $f);
 include $c;
 list($host, $uname, $pw, $db) = explode(":", $auth);
-// unset($cc);
 
 if($host==null || $uname==null || $db==null)
   die("Error: Podatci iz db.conf su prazni (?)");
